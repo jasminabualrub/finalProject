d36e33ffaa3ada112df4e80baa2d1c8fff5cf540
jasminabualrub <jasminabualrub1999@gmail.com>
2024-03-28 22:29:26 +0200

    edit info page

--- src/Pages/Profile/Component/info.jsx
+++ src/Pages/Profile/Component/info.jsx
@@ -1,52 +1,50 @@
-import { useEffect, useState } from 'react';
-import { toast } from 'react-toastify'
-import './Profile.css'
-import Loader from './../../../Components/Loader/Loader';
-import axios from 'axios';
-
+import { useEffect, useState } from "react";
+import { toast } from "react-toastify";
+import "./Profile.css";
+import Loader from "./../../../Components/Loader/Loader";
+import axios from "axios";
 
 function Info() {
-  const token=localStorage.getItem('userToken');
-  const [userInfo,setUserInfo] =useState([]);
-  const[loader,setLoader]=useState(true);
-  const[error,setError]=useState('');
-  const getInfo=async()=>{
-    try{
-      const {data}=await axios.get(`${import.meta.env.VITE_API_URL}/user/profile`,{headers:{Authorization:`Tariq__${token}`}})
+  const token = localStorage.getItem("userToken");
+  const [userInfo, setUserInfo] = useState([]);
+  const [loader, setLoader] = useState(true);
+  const [error, setError] = useState("");
+  const getInfo = async () => {
+    try {
+      const { data } = await axios.get(
+        `${import.meta.env.VITE_API_URL}/user/profile`,
+        { headers: { Authorization: `Tariq__${token}` } }
+      );
       setUserInfo(data.user);
 
-console.log(data.user)
+      console.log(data.user);
 
-  setError("");}
-    catch(err){
-      toast.error(err.response.data.message || 'Error to load your data :(')
-    }
-    finally{
+      setError("");
+    } catch (err) {
+      toast.error(err.response.data.message || "Error to load your data :(");
+    } finally {
       setLoader(false);
     }
+  };
+  useEffect(() => {
+    getInfo();
+  }, []);
+  if (loader) {
+    return <Loader />;
   }
-  useEffect(()=>{getInfo()},[]);
-  if(loader){
-    return <Loader />
-  }
-  return (<>
-     {error? <p>{error}</p> : null}
-   
-   {
-    <div  className='d-flex align-items-center  gap-3 m-3'key={userInfo.id}>
-      <div className='user-img'><img src={userInfo.image.secure_url} alt={userInfo.userName}/></div>
-      
-      <h5>{userInfo.userName}</h5>
-      
-      
-    </div>
-   
+  return (
+    <>
+      {error ? <p>{error}</p> : null}
 
-  } 
-  
+      <div className="d-flex align-items-center  gap-3 m-3" key={userInfo.id}>
+        <div className="user-img">
+          <img src={userInfo.image.secure_url} alt={userInfo.userName} />
+        </div>
+
+        <h5>{userInfo.userName}</h5>
+      </div>
     </>
- 
-  )
+  );
 }
 
-export default Info
+export default Info;
